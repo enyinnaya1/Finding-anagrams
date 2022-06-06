0d3e6cd1961043f8d67eefe9b797c24d33ea15bb
Precious Enyinnaya Achimba <63791800+enyinnaya1@users.noreply.github.com>
2022-06-06 02:26:29 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,24 @@
+# Anagram checker program(anagram words)
+
+def find_anagram(word, anagram):
+    if len(word) == len(anagram):
+        count = 0
+        for o in anagram:
+            check = o in word
+            if check:
+                count = count + 1
+            else:
+                return False
+                break
+        if count == len(anagram):
+            return True
+        else:
+            return False
+    else:
+        return False
+
+
+a = input("enter the word: ")
+b = input("enter the anagram: ")
+
+print(find_anagram(a.strip(), b.strip()))
